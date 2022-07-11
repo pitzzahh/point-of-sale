7928e410c496f5cc8ffb928635606c7588930cc3
pitzzahh <araopj2191@gmail.com>
2022-07-11 14:55:26 +0800

    removed createFile() and writeFile() methods.

--- src/main/java/com/github/pitzzahh/validation/Checker.java
+++ src/main/java/com/github/pitzzahh/validation/Checker.java
@@ -1,7 +1,5 @@
 package com.github.pitzzahh.validation;
 
-import java.io.*;
-
 /**
  * Class used to check whether the products have been added to the table in the database.
  * Also used for some input checking.
@@ -9,25 +7,6 @@ import java.io.*;
  * @author peter
  */
 public class Checker {
-
-    /**
-     * Creates a file that denotes that the products have been added to the table from the database.
-     */
-    public static void createFile() {
-        try {
-            File file = new File("C:\\Users\\Public\\check.txt");
-            if (!file.exists()) writeToFile(file);
-        } catch (IOException ignored) {}
-    }
-
-    private static void writeToFile(File file) throws IOException {
-        boolean created = file.createNewFile();
-        if (created) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-                bufferedWriter.write("This file is used to check whether the products are in the database, just checks if this file exists.\nDO NOT REMOVE THIS FILE, IF THIS FIL IS REMOVED, THE PROGRAM WILL KEEP INSERTING PRODUCTS TO THE TABLE");
-            }
-        }
-    }
 
     /**
      * Method that checks if a {@code String} is an Integer
