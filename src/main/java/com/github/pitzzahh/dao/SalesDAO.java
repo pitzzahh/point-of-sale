f877b5f1f05dac734e32c32bd78cba1de1545b4d
pitzzahh <araopj2191@gmail.com>
2022-07-11 15:11:09 +0800

    re-arranged imports.

--- src/main/java/com/github/pitzzahh/dao/SalesDAO.java
+++ src/main/java/com/github/pitzzahh/dao/SalesDAO.java
@@ -1,12 +1,10 @@
 package com.github.pitzzahh.dao;
 
+import com.github.pitzzahh.entity.Sales;
+import com.github.pitzzahh.enums.Status;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
-
-import com.github.pitzzahh.entity.Sales;
-import com.github.pitzzahh.enums.Status;
-
 import javax.sql.DataSource;
 import java.util.List;
 
--- src/main/java/com/github/pitzzahh/dao/SalesDAOImplementation.java
+++ src/main/java/com/github/pitzzahh/dao/SalesDAOImplementation.java
@@ -1,10 +1,9 @@
 package com.github.pitzzahh.dao;
 
-import com.github.pitzzahh.entity.Sales;
-import com.github.pitzzahh.enums.Status;
 import com.github.pitzzahh.service.ProductService;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import com.github.pitzzahh.entity.Sales;
+import com.github.pitzzahh.enums.Status;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
